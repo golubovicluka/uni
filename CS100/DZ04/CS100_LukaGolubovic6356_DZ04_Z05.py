71b9093e7afb52d3351167689ea2c230c183b899
Luka Golubovic <62186386+golubovicluka@users.noreply.github.com>
2023-12-15 23:49:58 +0100

    added DZ04 Z04 & Z05

---
+++ CS100/DZ04/CS100_LukaGolubovic6356_DZ04_Z05.py
@@ -0,0 +1,7 @@
+input_broj = int(input("Unesite pozitivan ceo broj: "))
+
+faktorijel = 1
+for i in range(1, input_broj+1):
+    faktorijel *= i
+
+print(f"Faktorijel broja {input_broj} je: {fact}")
